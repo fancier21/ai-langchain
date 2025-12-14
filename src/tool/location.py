3f3db76b8de7c7726f3ed6195926d4a18b755bb1
fancier <fancier@proton.me>
2025-12-14 13:20:59 +0400

    tools

--- src/tool/location.py
+++ src/tool/location.py
@@ -3,8 +3,7 @@ from langchain.tools import ToolRuntime, tool
 from model.context import Context
 
 
-@tool
+@tool("get_user_location", description="Retrieve user location based on user ID.")
 def get_user_location(runtime: ToolRuntime[Context]) -> str:
-    """Retrieve user information based on user ID."""
     user_id = runtime.context.user_id
     return "Florida" if user_id == "1" else "SF"
--- src/tool/weather.py
+++ src/tool/weather.py
@@ -1,7 +1,22 @@
+import os
+
+import requests
+from dotenv import load_dotenv
 from langchain.tools import tool
 
+load_dotenv()
 
-@tool
+
+WTTR_API_KEY = os.getenv("WTTR_API_KEY")
+url = "https://wttr.in"
+
+
+@tool("get_weather_for_location", description="Get weather for a given city.")
 def get_weather_for_location(city: str) -> str:
-    """Get weather for a given city."""
-    return f"It's always sunny in {city}!"
+    response = requests.get(f"{url}/{city}?format=j1")
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return f"The weather in {city} is {data['current_condition'][0]['weatherDesc'][0]['value']}."
+    else:
+        return f"Failed to fetch weather data for {city}. Please check your API key."
