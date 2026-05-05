e9eecbed74832a75a0500fd311a20711d3cc7c26
fancier <fancier@proton.me>
2026-05-05 11:57:31 +0400

    fixed tool

--- src/tool/weather.py
+++ src/tool/weather.py
@@ -1,13 +1,6 @@
-import os
-
 import requests
-from dotenv import load_dotenv
 from langchain.tools import tool
 
-load_dotenv()
-
-
-WTTR_API_KEY = os.getenv("WTTR_API_KEY")
 url = "https://wttr.in"
 
 
@@ -16,7 +9,6 @@ def get_weather_for_location(city: str) -> str:
     response = requests.get(f"{url}/{city}?format=j1")
     if response.status_code == 200:
         data = response.json()
-        print(data)
         return f"The weather in {city} is {data['current_condition'][0]['weatherDesc'][0]['value']}."
     else:
         return f"Failed to fetch weather data for {city}. Please check your API key."
